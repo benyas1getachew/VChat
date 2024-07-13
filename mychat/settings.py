4b7a3e46bb4dc50671d0731c863db9ab9cce5fd4
benyas1getachew <99409114+benyas1getachew@users.noreply.github.com>
2024-07-13 09:27:42 +0530

    Update settings.py

--- mychat/settings.py
+++ mychat/settings.py
@@ -125,8 +125,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# WhiteNoise settings
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
