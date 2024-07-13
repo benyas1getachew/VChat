443b4edec0ec2ac31d646186754a4cd3472201fb
benyas1getachew <99409114+benyas1getachew@users.noreply.github.com>
2024-07-13 09:57:37 +0530

    Update settings.py

--- mychat/settings.py
+++ mychat/settings.py
@@ -116,14 +116,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # Ensure this starts with a leading slash
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static',  # Directory where your static files are located
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # WhiteNoise settings
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
